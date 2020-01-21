439fe00343251fa7f9014586469601ad37993919
simona <palumbosimona118@gmail.com>
2020-01-21 19:05:59 +0100

    timer

--- js/script.js
+++ js/script.js
@@ -8,7 +8,9 @@
 
 alert('ciao');
 
+//Un alert espone 5 numeri casuali
 var arreyNumeriRandom = [];
+var arreyNumeriPrompt = [];
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -20,3 +22,18 @@ for (var i = 1; i < 6; i++) {
   arreyNumeriRandom.push(getRandomIntInclusive(1, 100));
 }
 alert(arreyNumeriRandom);
+
+//Da li parte un timer di 30 secondi
+//Dopo 30 secondi l'utente deve inserire un prompt alla volta
+// i numeri che ha visto precedentemente
+
+var i = 0;
+
+setTimeout(function() {
+  while (i < 5) {
+    var numeroUtente = parseInt(prompt('inserire un numero'));
+    arreyNumeriPrompt.push(numeroUtente);
+    console.log(arreyNumeriPrompt);
+  }
+  i++
+})
